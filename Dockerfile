3a927fb35ccc317f8d1c27f81f32ed53269aaa15
wolender <wolender@griddynamics.com>
2023-08-22 12:50:19 +0200

    adding dockerfile and jenkinsfile

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:17
+RUN mkdir /usr/src/petclinic
+WORKDIR /usr/src/petclinic
+COPY target/*.jar /usr/src/petclinic/app.jar
+RUN chmod 777 *.jar
+EXPOSE 80
+CMD ["java","-jar","app.jar"]
---
+++ build_app.groovy
@@ -0,0 +1,48 @@
+pipeline {
+    agent any
+
+
+    stages {
+        stage('Pull') {
+            steps {
+                git branch: 'main', url:'https://github.com/wolender/spring-petclinic.git'
+            }
+        }
+
+        stage('Static Code Analysis') {
+            steps {
+                sh 'echo analize'
+            }
+            
+        }
+
+        stage('Test') {
+            steps {
+                sh 'mvn clean test'
+
+                junit 'target/surefire-reports/*.xml'
+            }
+            
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean install -DskipTests'
+                sh 'docker build -t wolender-ecr .'
+            }
+            
+        }
+
+        stage('Push') {
+            steps {
+
+                load "$JENKINS_HOME/env_variables.groovy"
+
+                sh 'docker tag wolender-ecr:latest ${env.REPO_URL}'
+                sh 'docker push ${env.REPO_URL}'
+            }
+            
+        }
+
+    }
+}
