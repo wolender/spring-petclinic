bc17311dccdd36168f35f4dc0040ed63e980bfbc
wolender <wolender@griddynamics.com>
2023-08-22 16:03:22 +0200

    fix

--- deploy_app.groovy
+++ deploy_app.groovy
@@ -14,8 +14,8 @@ pipeline {
                 dir('deploy'){
                     load "$JENKINS_HOME/env_variables.groovy"
                     
-                    sh 'echo "ec2-user@${env.APP_IP}"'
-                    sh 'ansible-playbook deploy_playbook.yaml -e "REPO_URL=${env.REPO_URL}" '
+                    sh "echo ec2-user@${env.APP_IP} > inventory "
+                    sh "ansible-playbook deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" "
                 }
 
             }
