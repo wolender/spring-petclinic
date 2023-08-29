53ecf4255479a7b7335a63cdb03592ce2da6817d
wolender <wolender@griddynamics.com>
2023-08-29 13:45:10 +0200

    adding logging

--- deploy_app.groovy
+++ deploy_app.groovy
@@ -15,7 +15,7 @@ pipeline {
                     load "$JENKINS_HOME/env_variables.groovy"
                     
                     sh "echo ec2-user@${env.APP_IP} > inventory "
-                    sh "/var/lib/jenkins/.local/bin/ansible-playbook deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" "
+                    sh "/var/lib/jenkins/.local/bin/ansible-playbook -v deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" "
                     sh "echo http://${env.APP_IP}:8080"
                 }
 
