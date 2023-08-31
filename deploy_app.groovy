9bfa9bc122fc8b405ef6a4ae5607988b8bcf88c2
wolender <wolender@griddynamics.com>
2023-08-31 14:54:17 +0200

    adding db env

--- deploy_app.groovy
+++ deploy_app.groovy
@@ -15,8 +15,8 @@ pipeline {
                     load "$JENKINS_HOME/env_variables.groovy"
                     
                     sh "echo ec2-user@${env.APP_IP} > inventory "
-                    sh "/var/lib/jenkins/.local/bin/ansible-playbook -v deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" "
-                    sh "echo http://${env.APP_IP}:8080"
+                    sh "/var/lib/jenkins/.local/bin/ansible-playbook -v deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" -e \"MYSQL_URL=${env.MYSQL_URL}\" -e \"MYSQL_USER=${env.MYSQL_USER}\""
+                    sh "echo http://${env.APP_LB_URL}"
                 }
 
             }
