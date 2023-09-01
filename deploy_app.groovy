c62e91d6cbc4864161965a2f356a65d4cd0ed6c1
wolender <wolender@griddynamics.com>
2023-09-01 15:38:22 +0200

    fix

--- deploy_app.groovy
+++ deploy_app.groovy
@@ -13,9 +13,10 @@ pipeline {
             steps {
                 dir('deploy'){
                     load "$JENKINS_HOME/env_variables.groovy"
+                    load "$JENKINS_HOME/app_version.groovy"
                     
                     sh "echo ec2-user@${env.APP_IP} > inventory "
-                    sh "/var/lib/jenkins/.local/bin/ansible-playbook -v deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" -e \"MYSQL_URL=${env.MYSQL_URL}\" -e \"MYSQL_USER=${env.MYSQL_USER}\""
+                    sh "/var/lib/jenkins/.local/bin/ansible-playbook -v deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" -e \"MYSQL_URL=${env.MYSQL_URL}\" -e \"MYSQL_USER=${env.MYSQL_USER}\" -e \"APP_NEW_VER=${env.APP_NEW_VER}\""
                     sh "echo http://${env.APP_LB_URL}"
                 }
 
