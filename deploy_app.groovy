6e44c99b5de426d92f68f9b619205cd6f2c1061f
wolender <wolender@griddynamics.com>
2023-09-11 17:15:04 +0200

    adding artifacts

--- deploy_app.groovy
+++ deploy_app.groovy
@@ -12,16 +12,22 @@ pipeline {
             }
         }
 
+        stage ('Copy Variables'){
+            steps{
+                copyArtifacts(
+                    projectName: 'Provision Resources', // Name of the first pipeline
+                    filter: 'env_variables.groovy', // Path to the artifact in the first pipeline
+                    target: "vars" 
+                )
+            }
+        }
+
         stage('Deploy via Ansible') {
             steps {
                 dir('deploy'){
-
-                    copyArtifacts fingerprintArtifacts: true,
-                    projectName: "${dump_job_path}",
-                    target: "env_vars.groovy"
                     
-                    load "$JENKINS_HOME/env_variables.groovy"
-                    load "$JENKINS_HOME/app_version.groovy"
+                    load "vars/env_variables.groovy"
+                    load "vars/app_version.groovy"
                     
                     sh "echo ec2-user@${env.APP_IP} > inventory "
                     sh "/var/lib/jenkins/.local/bin/ansible-playbook -v deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" -e \"MYSQL_URL=${env.MYSQL_URL}\" -e \"MYSQL_USER=${env.MYSQL_USER}\" -e \"APP_NEW_VER=${env.APP_NEW_VER}\""
