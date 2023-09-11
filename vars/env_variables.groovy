e50500d2629f5aacb45af8e51668a4f80ff2d51a
EC2 Default User <ec2-user@ip-10-0-1-197.eu-central-1.compute.internal>
2023-09-11 15:59:19 +0000

    Version: 3.14.1

--- vars/env_variables.groovy
+++ vars/env_variables.groovy
@@ -1,5 +1,5 @@
-env.APP_IP="35.159.25.238"
+env.APP_IP="3.76.222.127"
 env.REPO_URL="113304117666.dkr.ecr.eu-central-1.amazonaws.com/wolender-ecr"
-env.MYSQL_URL="terraform-20230911125457871000000001.cv7gyy9x6999.eu-central-1.rds.amazonaws.com:3306"
-env.APP_LB_URL="wolender-web-app-lb-1603328723.eu-central-1.elb.amazonaws.com"
+env.MYSQL_URL="terraform-20230911155122940500000001.cv7gyy9x6999.eu-central-1.rds.amazonaws.com:3306"
+env.APP_LB_URL="wolender-web-app-lb-900006920.eu-central-1.elb.amazonaws.com"
 env.MYSQL_USER="petclinic"
