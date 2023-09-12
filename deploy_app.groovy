20a8299c23b142d42cb529ec26d5794ecfa6787a
wolender <wolender@griddynamics.com>
2023-09-12 13:25:48 +0200

    fix

--- deploy_app.groovy
+++ deploy_app.groovy
@@ -32,7 +32,7 @@ pipeline {
                         
                         sh "echo ec2-user@${env.APP_IP} > inventory "
                         sh "/var/lib/jenkins/.local/bin/ansible-playbook -v deploy_playbook.yaml -e \"REPO_URL=${env.REPO_URL}\" -e \"MYSQL_URL=${env.MYSQL_URL}\" -e \"MYSQL_USER=${USERNAME}\" -e \"MYSQL_PASS=${PASSWORD}\" -e \"APP_NEW_VER=${env.APP_NEW_VER}\""
-                        currentBuild.description = "Application URL: [http://${env.APP_LB_URL}](http://${env.APP_LB_URL})"
+                        
                     }
                 }
 
@@ -40,5 +40,14 @@ pipeline {
             
         }
 
+    }
+    post {
+        success {
+            script {
+                load "../vars/env_variables.groovy"
+                def appUrl = "http://${env.APP_LB_URL}" // Replace with your actual app URL
+                currentBuild.description = "Application URL: [${appUrl}](${appUrl})"
+            }
+        }
     }
 }
